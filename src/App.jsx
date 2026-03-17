536d403661747f20e66125a55df77f97b2c67754
LingkonDash <cocrupon@gamil.com>
2026-03-17 22:55:50 +0600

    restructured project files and added animation in navbar dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
+import { Suspense } from 'react';
+import Navbar from './components/navbar/Navbar';
+import SortNav from './components/Sort&navigation/SortNav';
+import Flags from './components/flags/Flags';
 
 
 function App() {
 
+
+  const apiPromise = fetch('https://openapi.programming-hero.com/api/all').then(res => res.json());
+
   return (
     <>
 
-      <Header></Header>
+      <header className='sticky top-0 bg-base-300 z-10'>
+        <Navbar></Navbar>
+      </header>
 
-      <Main></Main>
+      <main className='max-w-[1600px] mx-auto'>
+        <SortNav></SortNav>
 
+        <Suspense fallback={<p>Data Loading...</p>}>
+          <Flags apiPromise={apiPromise}></Flags>
+        </Suspense>
+      </main>
+
+      <footer className='max-w-[1600px] mx-auto'>
+
+      </footer>
 
 
     </>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
 
 
   return (
-    <nav>
-      <div className="navbar py-5 px-4">
+    <nav className='max-w-[1600px] mx-auto'>
+      <div className="navbar pt-5 pb-2 px-4 bg-base-300">
         <div className="navbar-start">
-          <div className='max-w-[315px] min-w-[280px]'>
+          <div className='md:w-[315px] w-[281px] min-w-[281px]'>
             <a href="#">
               <img src={logo} alt="Logo" />
             </a>
@@ -44,25 +44,26 @@ const Navbar = () => {
         </button>
       </div>
       {/* dropdown menu */}
-      <div className={`${dropDown ? 'flex' : 'hidden'} md:hidden justify-center items-center gap-2 pb-5 h-8`}>
+      <div className={`absolute top-0 left-0 w-full flex bg-base-300 md:hidden justify-center items-center gap-2 pb-5 pt-1 transition-all duration-300 -z-10
+                    ${dropDown ? 'translate-y-[65px]' : ' -translate-y-5 pointer-events-none'}`}>
 
         <label className="input shadow-sm focus-within:outline-[#3333] h-8">
-            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
-              >
-                <circle cx="11" cy="11" r="8"></circle>
-                <path d="m21 21-4.3-4.3"></path>
-              </g>
-            </svg>
-            <input type="search" className="grow" placeholder="Search" />
-          </label>
+          <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input type="search" className="grow" placeholder="Search" />
+        </label>
 
-          <Search className='text-[#3075f4] cursor-pointer' />
+        <Search className='text-[#3075f4] cursor-pointer' />
 
       </div>
     </nav>
