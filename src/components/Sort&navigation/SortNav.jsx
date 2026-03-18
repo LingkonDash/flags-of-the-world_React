7f9162e1728a5a5f613ff966d05251f98ed82d43
LingkonDash <cocrupon@gamil.com>
2026-03-19 03:12:08 +0600

    Added home bars in small devices

--- src/components/Sort&navigation/SortNav.jsx
+++ src/components/Sort&navigation/SortNav.jsx
@@ -1,16 +1,21 @@
 import { ChevronDown } from 'lucide-react';
-import React from 'react';
+import React, { useState } from 'react';
 
 const SortNav = () => {
+  
+  const [continents, setContinents] = useState(false);
+
+  const [sort, setSort] = useState(false);
+
   return (
     <section className='flex justify-between items-center'>
-      <div className='btn text-xl font-normal cursor-pointer py-1 px-3 shadow-none rounded-full border-4 border-transparent hover:border-[#3075f469] bg-base-300'>
-        <h2> Sort </h2> <ChevronDown />
-      </div>
+      <button onClick={() => setSort(!sort)} className={`btn text-xl font-normal cursor-pointer py-1 px-3 shadow-none rounded-full border-4 border-transparent lg:hover:border-[#3075f469] bg-base-300 ${sort ? 'border-[#3075f469]' : ''}`}>
+        Sort <ChevronDown />
+      </button>
       <div>
-        <div className='lg:hidden btn text-xl font-normal cursor-pointer py-1 px-3 shadow-none rounded-full border-4 border-transparent hover:border-[#3075f469] bg-base-300'>
-          <h2> Continents </h2> <ChevronDown />
-        </div>
+        <button onClick={() => setContinents(!continents)} className={`lg:hidden btn text-xl font-normal cursor-pointer py-1 px-3 shadow-none rounded-full border-4 border-transparent lg:hover:border-[#3075f469] bg-base-300 ${continents ? 'border-[#3075f469]' : ''}`}>
+          Continents <ChevronDown />
+        </button>
         <ul className='hidden lg:flex justify-center items-center gap-5 text-blue-600 font-normal text-xl'>
           <li><p className='hover:text-blue-800 cursor-pointer'>Asia</p></li>
           <li><p className='hover:text-blue-800 cursor-pointer'>Africa</p></li>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,6 +8,8 @@ const Navbar = () => {
 
   const [dropDown, setDropdown] = useState(false);
 
+  const [dropDownNav, setDropdownNav] = useState(false);
+
 
   return (
     <nav className='max-w-[1600px] mx-auto'>
@@ -25,12 +27,36 @@ const Navbar = () => {
         <div className="navbar-end hidden md:flex justify-between items-center gap-10">
           <SearchBar></SearchBar>
 
-          <Navigations ></Navigations>
+          <div className='pr-4 hidden xl:flex'>
+            <Navigations ulStyle={'flex justify-between items-center gap-10 text-xl font-medium [&_li]:border-4 [&_li]:border-transparent'}></Navigations>
+          </div>
+
+          <div className='hidden md:flex xl:hidden navbar-end'>
+            <button onClick={() => setDropdownNav(!dropDownNav)} className={`btn bg-base-300 shadow-none cursor-pointer py-1 px-3 rounded-full border-4 border-transparent hover:border-[#3075f469] ${dropDownNav ? 'border-[#3075f469]' : ''}`}>
+              {
+                dropDownNav
+                  ? <X width={'20px'}></X>
+                  : <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+              }
+
+            </button>
+
+            <div className={`absolute top-0 right-5 w-fit bg-base-300 hidden md:flex xl:hidden justify-center items-center pb-5 pl-5 pt-1 transition-transform duration-300 -z-10 shadow-xl rounded-b-lg border-4 text-center border-[#3075f469]
+                    ${dropDownNav ? 'translate-y-[65px]' : ' -translate-y-[500px] pointer-events-none'}`}>
+
+
+              <div className='pr-4'>
+                <Navigations ulStyle={'flex flex-col justify-between items-center gap-10 text-xl font-medium [&_li]:border-4 [&_li]:border-transparent'}></Navigations>
+              </div>
+            </div>
+
+          </div>
+
         </div>
 
         {/* Dropdown button */}
         <div className='md:hidden navbar-end'>
-          <button onClick={() => setDropdown(!dropDown)} className='btn bg-base-300 shadow-none cursor-pointer py-1 px-3 rounded-full border-4 border-transparent hover:border-[#3075f469]'>
+          <button onClick={() => setDropdown(!dropDown)} className={`btn bg-base-300 shadow-none cursor-pointer py-1 px-3 rounded-full border-4 border-transparent ${dropDown ? 'border-[#3075f469]' : ''}`}>
             {
               dropDown
                 ? <X width={'20px'}></X>
@@ -43,10 +69,14 @@ const Navbar = () => {
       </div>
 
       {/* dropdown menu */}
-      <div className={`absolute top-0 left-0 w-full flex bg-base-300 md:hidden justify-center items-center pb-5 pt-1 transition-transform duration-300 -z-10
-                    ${dropDown ? 'translate-y-[65px]' : ' -translate-y-5 pointer-events-none'}`}>
+      <div className={`absolute top-0 left-0 w-full flex flex-col bg-base-300 md:hidden justify-center items-center pb-5 pt-1 transition-transform duration-300 -z-10
+                    ${dropDown ? 'translate-y-[65px]' : ' -translate-y-20 pointer-events-none'}`}>
 
         <SearchBar></SearchBar>
+
+        <div className='pr-4 md:hidden mt-5'>
+            <Navigations ulStyle={'flex justify-between items-center gap-10 text-xl font-medium [&_li]:border-4 [&_li]:border-transparent'}></Navigations>
+          </div>
       </div>
 
     </nav>
