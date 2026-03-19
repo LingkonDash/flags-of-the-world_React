49bc210273f95e1d258518bb3cea6ac9d10404ec
LingkonDash <cocrupon@gamil.com>
2026-03-19 20:32:56 +0600

    Added sortList

--- src/components/Sort&navigation/SortNav.jsx
+++ src/components/Sort&navigation/SortNav.jsx
@@ -1,11 +1,26 @@
 import { ChevronDown } from 'lucide-react';
 import React, { useState } from 'react';
 import Continents from './continent/Continents';
+import SortList from './sortList/SortList';
 
 const SortNav = () => {
 
   const [continents, setContinents] = useState(false);
   const [currentContinent, setCurrentContinent] = useState('');
+  const [sort, setSort] = useState(false);
+  const [curSort, setCurSort] = useState('');
+
+  const handleSort = (sortName) => {
+
+    if (sortName === 'Asia') setCurSort('Asia');
+    if (sortName === 'Africa') setCurSort('Africa');
+    if (sortName === 'northA') setCurSort('northA');
+    if (sortName === 'southA') setCurSort('southA');
+    if (sortName === 'Antarctica') setCurSort('Antarctica');
+    if (sortName === 'Europe') setCurSort('Europe');
+    if (sortName === 'Australia') setCurSort('Australia');
+    console.log(sortName);
+  }
 
   const handleContinent = (continentName) => {
     if (continentName === 'Asia') setCurrentContinent('Asia');
@@ -17,13 +32,18 @@ const SortNav = () => {
     if (continentName === 'Australia') setCurrentContinent('Australia');
   }
 
-  const [sort, setSort] = useState(false);
 
   return (
     <section className='flex justify-between items-center'>
-      <button onClick={() => setSort(!sort)} className={`btn text-xl font-normal cursor-pointer py-1 px-3 shadow-none rounded-full border-4 lg:hover:border-[#3075f469] bg-base-300 ${sort ? 'border-[#3075f469]' : 'border-transparent'}`}>
-        Sort <ChevronDown />
-      </button>
+
+      <div className='relative'>
+        <button onClick={() => setSort(!sort)} className={`btn text-xl font-normal cursor-pointer py-1 px-3 shadow-none rounded-full border-4 lg:hover:border-[#3075f469] bg-base-300 ${sort ? 'border-[#3075f469]' : 'border-transparent'}`}>
+          Sort <ChevronDown />
+        </button>
+
+        <SortList handleSort={handleSort} curSort={curSort} ulStyle={`${sort ? 'translate-x-0' : ' -translate-x-[300px] pointer-events-none opacity-0'}`}></SortList>
+
+      </div>
       <div className='relative'>
         <button onClick={() => setContinents(!continents)} className={`lg:hidden btn text-xl font-normal cursor-pointer py-1 px-3 shadow-none rounded-full border-4 lg:hover:border-[#3075f469] bg-base-300 ${continents ? 'border-[#3075f469]' : 'border-transparent'}`}>
           Continents <ChevronDown />
@@ -32,10 +52,10 @@ const SortNav = () => {
         <Continents handleContinent={handleContinent} currentContinent={currentContinent} ulStyle={'hidden lg:flex'}></Continents>
 
         {
-          <Continents handleContinent={handleContinent} currentContinent={currentContinent} ulStyle={`flex lg:hidden flex-col rounded-lg border-4 border-r-transparent border-[#3075f469] z-10 bg-base-200 py-6 absolute top-12 shadow-md -right-7 left-0 transition-transform duration-300 ${continents ? 'translate-x-0' : ' translate-x-[200px] pointer-events-none'} `}></Continents>
+          <Continents handleContinent={handleContinent} currentContinent={currentContinent} ulStyle={`flex lg:hidden flex-col rounded-lg border-4 border-[#3075f469] z-10 bg-base-200 py-6 absolute top-12 shadow-md -right-7 left-0 transition-transform duration-300 ${continents ? 'translate-x-0' : ' translate-x-[200px] pointer-events-none'}`}></Continents>
         }
 
-        
+
       </div>
     </section>
   );
---
+++ src/components/Sort&navigation/sortList/SortList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SortList = ({handleSort, curSort, ulStyle}) => {
+  return (
+    <ul className={`${ulStyle} rounded-lg border-4 border-[#3075f469] z-10 bg-base-200 py-6 absolute top-12 shadow-md -right-[100px] -left-7 transition-transform duration-300 flex flex-col justify-center items-start pl-5 gap-5 text-blue-600 font-normal text-xl`}>
+      <li><button onClick={() => handleSort('Asia')} className={`hover:text-blue-800 cursor-pointer active:scale-95 border-4 border-transparent ${curSort === 'Asia' ? 'border-b-[#3075f469]' : 'border-b-transparent'}`}>Asia</button></li>
+      <li><button onClick={() => handleSort('Africa')} className={`hover:text-blue-800 cursor-pointer active:scale-95 border-4 border-transparent ${curSort === 'Africa' ? 'border-b-[#3075f469]' : 'border-b-transparent'}`}>Africa</button></li>
+      <li><button onClick={() => handleSort('northA')} className={`hover:text-blue-800 cursor-pointer active:scale-95 border-4 border-transparent ${curSort === 'northA' ? 'border-b-[#3075f469]' : 'border-b-transparent'}`}>North America</button></li>
+      <li><button onClick={() => handleSort('southA')} className={`hover:text-blue-800 cursor-pointer active:scale-95 border-4 border-transparent ${curSort === 'southA' ? 'border-b-[#3075f469]' : 'border-b-transparent'}`}>South America</button></li>
+      <li><button onClick={() => handleSort('Antarctica')} className={`hover:text-blue-800 cursor-pointer active:scale-95 border-4 border-transparent ${curSort === 'Antarctica' ? 'border-b-[#3075f469]' : 'border-b-transparent'}`}>Antarctica</button></li>
+      <li><button onClick={() => handleSort('Europe')} className={`hover:text-blue-800 cursor-pointer active:scale-95 border-4 border-transparent ${curSort === 'Europe' ? 'border-b-[#3075f469]' : 'border-b-transparent'}`}>Europe</button></li>
+      <li><button onClick={() => handleSort('Australia')} className={`hover:text-blue-800 cursor-pointer active:scale-95 border-4 border-transparent ${curSort === 'Australia' ? 'border-b-[#3075f469]' : 'border-b-transparent'}`}>Australia</button></li>
+    </ul>
+  );
+};
+
+export default SortList;
